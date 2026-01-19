29d56543a51d405cd69b957f4d77b0ff83d09951
dragonews@orange.fr <dragonews@orange.fr>
2026-01-19 13:59:42 +0000

    récupération des données propres de hosts

--- models/curation/curation_host.sql
+++ models/curation/curation_host.sql
@@ -19,9 +19,12 @@ WITH hosts_raw AS (
         AND host_since >= '1900-01-01'
         AND host_since <= current_date()
         AND host_location IS NOT NULL
+        AND host_location RLIKE'^[a-zA-Z0-9_\\s]+,[a-zA-Z0-9_\\s]+$'
         AND host_is_superhost IS NOT NULL
+        AND (host_is_superhost LIKE 't' OR host_is_superhost LIKE 'f')
         AND host_neighbourhood IS NOT NULL
-		AND host_identity_verified IS NOT NULL 
+		AND host_identity_verified IS NOT NULL
+        AND (host_identity_verified LIKE 't' OR host_identity_verified LIKE 'f') 
     )
 SELECT *
 from hosts_raw
