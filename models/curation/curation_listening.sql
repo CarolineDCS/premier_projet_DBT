8bca939f78b59d7c12d1ead8bf07b4f45ff246b5
Caroline DEVRED <dragonews@orange.fr>
2026-01-27 14:42:56 +0000

    filtrage pour que le snapshot de litening_snapshot ne prenne que les données qui passent les tests de qualité.

--- models/curation/curation_listening.sql
+++ models/curation/curation_listening.sql
@@ -26,37 +26,7 @@ WITH
 		maximum_nights
 	FROM {{ ref("listening_snapshot")}}
     WHERE DBT_VALID_TO IS NULL
-       AND id IS NOT NULL
-       AND listing_url IS NOT NULL
-      AND LENGTH(listing_url) > 0
-        AND (listing_url LIKE 'https://%'
-        OR listing_url LIKE 'http://%')
-		AND name IS NOT NULL
-		AND host_id IN (SELECT host_id FROM to_join_verified) 
-		AND latitude IS NOT NULL
-        AND latitude >= -90.0
-        AND latitude <= 90.0
-        AND longitude >= -180.0
-        AND longitude <= 180.0
-		AND property_type IS NOT NULL
-		AND room_type IS NOT NULL
-		AND accommodates IS NOT NULL
-        AND accommodates > 0
-		AND bathrooms IS NOT NULL
-        AND bathrooms >=0
-        AND bedrooms IS NOT NULL
-        AND bedrooms >0
-		AND beds IS NOT NULL 
-        AND beds >0
-		AND amenities IS NOT NULL
-        AND price IS NOT NULL
-        AND price RLIKE  '^\\$[[:space:]]*[0-9]{1,3}(,[0-9]{3})*(\\.[0-9]{0,2})?$'
-        AND  trim(try_to_double(REPLACE(split_part(price, '$', 2), ',', ''))) >0
-        AND minimum_nights IS NOT NULL
-        AND minimum_nights > 0
-        AND maximum_nights IS NOT NULL
-        AND maximum_nights > 0
-		AND minimum_nights<= maximum_nights    
+    AND host_id IN (SELECT host_id FROM to_join_verified) 
     )
 SELECT *
 FROM listings_raw
--- snapshots/listening_snapshot.sql
+++ snapshots/listening_snapshot.sql
@@ -10,6 +10,37 @@
         )
     }}
 
-    select * from {{ source('raw_airbnb_data', "listings") }}
+    SELECT * 
+    FROM {{ source('raw_airbnb_data', "listings") }}
+    WHERE  id IS NOT NULL
+       AND listing_url IS NOT NULL
+      AND LENGTH(listing_url) > 0
+        AND (listing_url LIKE 'https://%'
+        OR listing_url LIKE 'http://%')
+		AND name IS NOT NULL
+		AND latitude IS NOT NULL
+        AND latitude >= -90.0
+        AND latitude <= 90.0
+        AND longitude >= -180.0
+        AND longitude <= 180.0
+		AND property_type IS NOT NULL
+		AND room_type IS NOT NULL
+		AND accommodates IS NOT NULL
+        AND accommodates > 0
+		AND bathrooms IS NOT NULL
+        AND bathrooms >=0
+        AND bedrooms IS NOT NULL
+        AND bedrooms >0
+		AND beds IS NOT NULL 
+        AND beds >0
+		AND amenities IS NOT NULL
+        AND price IS NOT NULL
+        AND price RLIKE  '^\\$[[:space:]]*[0-9]{1,3}(,[0-9]{3})*(\\.[0-9]{0,2})?$'
+        AND  trim(try_to_double(REPLACE(split_part(price, '$', 2), ',', ''))) >0
+        AND minimum_nights IS NOT NULL
+        AND minimum_nights > 0
+        AND maximum_nights IS NOT NULL
+        AND maximum_nights > 0
+		AND minimum_nights<= maximum_nights    
 
 {% endsnapshot %}
